f87396efb3ca7b83e5285fe5b29bc13eec92bc5f
ungsebi <ungsebi@gmail.com>
2022-09-27 09:56:41 +0900

    FrontPage 만든 것 올림

---
+++ Body-Info_PJT/body_info/src/main/java/com/middle_project/body_info/BodyInfoApplication.java
@@ -0,0 +1,13 @@
+package com.middle_project.body_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyInfoApplication.class, args);
+	}
+
+}
---
+++ Body-Info_PJT/body_info/src/test/java/com/middle_project/body_info/BodyInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.middle_project.body_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
