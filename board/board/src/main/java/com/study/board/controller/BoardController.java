4aee6746ece47dd08acfe1ebf938b04a384492b5
ONEHENA <biny159@gmail.com>
2022-09-29 18:11:12 +0900

    게시판

---
+++ board/board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,83 @@
+package com.study.board.controller;
+
+import com.study.board.empty.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write") //localhost:8090/board/write
+    public String boardWriteForm() {
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+
+        boardService.write(board);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model, @PageableDefault(page = 0,size = 10,sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<Board> list = boardService.boardList(pageable);
+
+        int nowPage = list.getPageable().getPageNumber()+1;
+        int startPage = Math.max(nowPage - 4 , 1);
+        int endPage = Math.min(nowPage+5,list.getTotalPages());
+
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage",nowPage);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id) {
+
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Model model, Integer id) {
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board) {
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
+    }
+
+}
